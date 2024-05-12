8854cdf8ef09f0a137974257bae5ae4e48a20921
Abader <abadeerdodoesmut@gmail.com>
2024-05-12 13:52:35 +0300

    Add project files.

---
+++ CarFactoryLibrary_Lab1_test/CarFactory_Lab_tests.cs
@@ -0,0 +1,72 @@
+﻿using CarFactoryLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFactoryLibrary_Lab1_test
+{
+    public class CarFactory_Lab_tests
+    {
+        [Fact]
+        public void NewCar_CarTypesBMW_BMW()
+        {
+            //Arrange
+            BMW BMW = new BMW(); 
+
+            //Act
+          Car? car=  CarFactory.NewCar(CarTypes.BMW);
+
+            Car car2 = BMW.GetMyCar();
+
+
+
+
+            //Assert
+            Assert.Equal(BMW, car);
+
+            Assert.Same(BMW, car2);
+            Assert.NotNull(car2);
+
+        
+
+        }
+        [Fact]
+        public void NewCar_CarTypeToyota_Toyota()
+        {
+            
+
+            //Act
+            Car? car = CarFactory.NewCar(CarTypes.Toyota);
+
+
+
+
+
+            //Assert
+  
+            Assert.IsType<Toyota>(car);
+            Assert.IsNotType<Car>(car);
+            Assert.IsNotType<BMW>(car);
+        }
+
+        [Fact]
+        public void NewCar_CarTypeBuggati_Exception()
+        {
+            //Assert
+
+
+
+
+
+            //Assert
+
+            Assert.Throws<NotImplementedException>(() =>
+            {
+                //Act
+                Car? car = CarFactory.NewCar(CarTypes.Buggati);
+            });
+        }
+    }
+}
---
+++ CarFactoryLibrary_Lab1_test/Lab_ToyotaTests.cs
@@ -0,0 +1,94 @@
+﻿using CarFactoryLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFactoryLibrary_Lab1_test
+{
+    public class Lab_ToyotaTests
+    {
+        [Fact]
+        public void IsStopped_Moving_False()
+        {
+            //Arrange 
+            Toyota toyota = new Toyota();
+            toyota.velocity = 5;
+
+            //Act
+
+            bool result = toyota.IsStopped();
+
+            //Assert
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TimeToCoverDistance_Time_30Dist10Veloctiy_3Time()
+        {
+            //Arrange 
+              
+            Toyota toyota = new Toyota();
+            toyota.velocity = 10;
+            double Distance = 30;
+
+            //Act
+
+            double Time = toyota.TimeToCoverDistance(Distance);
+
+            //Assert
+            Assert.InRange(Time, 1, 3);
+            Assert.NotInRange(Distance, 4, 5);
+        }
+
+
+        [Fact]
+        public void GetDirection_Backward_Backward()
+        {
+            //Arrange 
+            Toyota toyota = new Toyota();
+            toyota.drivingMode=DrivingMode.Backward;
+
+            //Act
+            string Direction = toyota.GetDirection();
+
+            //Assert
+            //1
+            Assert.StartsWith("B", Direction);
+            //2
+            Assert.EndsWith("d", Direction);
+            //3
+            Assert.Contains("a", Direction);
+            //4
+            Assert.DoesNotContain("b", Direction);
+          
+
+        }
+        [Fact]
+        public void GetDirection_Stopped_Stopped()
+        {
+            //Arrange 
+            Toyota toyota = new Toyota();
+            toyota.drivingMode = DrivingMode.Stopped;
+
+            //Act
+            string Direction = toyota.GetDirection();
+
+            //Assert
+            //1
+            Assert.StartsWith("S", Direction);
+            //2
+            Assert.EndsWith("d", Direction);
+            //3
+            Assert.Contains("p", Direction);
+            //4
+            Assert.DoesNotContain("a", Direction);
+
+
+        }
+
+      
+    }
+}
